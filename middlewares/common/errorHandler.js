5d14d568fe12a66f389b7d1a9161a0469d0fc4d6
John Saviour <johnsaviour88@gmail.com>
2022-09-05 10:23:49 +0100

    updated error handler

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -1,4 +1,5 @@
 const createError = require("http-errors")
+const {model} = require('mongoose')
 
 // 404 not foound handler
 
@@ -8,10 +9,22 @@ function notFoundHandler(req, res, next){
 
 // default error handler
 function errorHandler(err, req, res, next){
-    res.render('error', {
-        title: "Error Page",
+    res.locals.error = process.env.NODE_ENV === "development" ? err :
+    {message: err.message}
+    res.status(err.status || 500)
 
-    })
+    if(res.locals.html){
+        // html response
+        res.render("error", {
+            title: 'this is html error page',
+        })
+    }else{
+        // json response
+        res.json(res.locals.error)
+    }
+
+    // res.locals.title = "This is a error page"
+    // res.render('error') 
 }
 
 module.exports = {notFoundHandler, errorHandler}
